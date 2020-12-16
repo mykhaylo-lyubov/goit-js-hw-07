7508bc6a521bc6a58d0004980cc957fe68950ab7
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2020-12-16 10:44:29 +0200

    module 7 task no 5 has been modified according to mentors review

--- tasks/task-05.js
+++ tasks/task-05.js
@@ -2,8 +2,7 @@ const inputRef = document.querySelector("input#name-input");
 const nameOutputRef = document.querySelector("span#name-output");
 
 inputRef.addEventListener("input", (event) => {
-  nameOutputRef.textContent = event.target.value;
-  if (event.target.value === "") {
-    nameOutputRef.textContent = "незнакомец";
-  }
+  event.target.value !== ""
+    ? (nameOutputRef.textContent = event.target.value)
+    : (nameOutputRef.textContent = "незнакомец");
 });
