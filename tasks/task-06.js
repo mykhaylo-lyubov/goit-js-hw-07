2c26b992b7d670bbd664c90babcbb2d6272b87b8
Mykhaylo Lyubov <mykhaylo.lyubov@gmail.com>
2020-12-16 10:49:59 +0200

    module 7 task no 6 has been modified according to mentors review

--- tasks/task-06.js
+++ tasks/task-06.js
@@ -3,6 +3,7 @@ const requiredNumberOfSymbols = inputToValidateRef.getAttribute("data-length");
 
 const checkForValidInput = (event) => {
   inputToValidateRef.classList.add("invalid");
+  inputToValidateRef.classList.remove("valid");
   if (event.target.value.length === Number(requiredNumberOfSymbols)) {
     inputToValidateRef.classList.remove("invalid");
     inputToValidateRef.classList.add("valid");
